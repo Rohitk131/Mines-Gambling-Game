f089b36b64f9970c8e215509552a4d5634f70da1
Rohitk131 <rohitkumar13103@gmail.com>
2024-06-28 23:46:27 +0530

    fix tailwind config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -6,7 +6,7 @@ const {
 const config: Config = {
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/Components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
